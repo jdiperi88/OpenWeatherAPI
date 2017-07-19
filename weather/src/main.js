4d6fde9057f0a666def07c847df4fb77928f0cfe
Joseph DiPeri <jdiperi88@gmail.com>
2017-07-19 17:18:42 -0400

    finished  project

--- weather/src/main.js
+++ weather/src/main.js
@@ -17,3 +17,59 @@ STEPS
   - append the data to the DOM
 
 */
+
+
+    
+  
+$('.btn').on('click',function(){
+     let zip = parseInt($('.zip').val());
+      getWeather(zip);
+      $('#container2').css('display','block')
+      $('#container2').css('display','flex')
+      $('#pic').css('display','block')
+    })
+
+
+
+// $('#btn2').on('click',function(){
+//      let number = parseInt($('#zip').val());
+//       getWeather(number);
+// })
+
+
+   
+
+function getWeather(zip) {
+    
+    console.log(zip);
+    var weather = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&units=imperial&appid=f065210a08e2910822646f8b726f6333`;
+    $.ajax({
+      method: "GET",
+      url: weather,
+      success: function(data){
+        console.log(data);
+        let currentTemp = data.main.temp;
+        let max = data.main.temp_max;
+        let min = data.main.temp_min;
+        let city = data.name;
+        let description = data.weather[0].description;
+        let image = data.weather[0].icon;  
+        $('#pic').attr('src',`http://openweathermap.org/img/w/${image}.png`);
+        $(".city").text(city);
+        $(".row3").text(`${Math.floor(currentTemp)}° degrees`);
+        $(".row4").text(`${description}`);
+        $(".minMax1").text(`Max: ${max}°`);
+        $(".minMax2").text(`Min: ${min}°`);
+   
+      //  let max = temperature;
+   
+      }
+      
+    });
+}
+
+
+
+
+
+
